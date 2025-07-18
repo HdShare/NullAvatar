c93e009c51a164fb8bc487675f3a001bc9b054fd
hd <hdshare@vip.qq.com>
2025-07-18 22:12:50 +0800

    chore: update 10-1.9 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.hd.nullavatar"
         minSdk = 27
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
         buildConfigField("String", "APP_NAME", "\"NullAvatar\"")
     }
 
